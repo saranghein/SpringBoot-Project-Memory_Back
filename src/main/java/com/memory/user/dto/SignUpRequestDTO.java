fdbe1db55d5af9ac3b51c1fd52ca1ea4b4628702
5jun99 <oj041088@gmail.com>
2024-08-06 20:29:10 +0900

    [fix] 글자 수 제한, webconfig 수정

--- src/main/java/com/memory/user/dto/SignUpRequestDTO.java
+++ src/main/java/com/memory/user/dto/SignUpRequestDTO.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public class SignUpRequestDTO {
     @NotBlank(message = "이름은 필수 입력 값입니다.")
-    @Size(min = 3, max = 30, message = "이름은 3자 이상, 20자 이하로 입력해주세요.")
+    @Size(min = 2, max = 30, message = "이름은 2자 이상, 20자 이하로 입력해주세요.")
     private String userName;
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
     @Size(min = 4, max = 20, message = "아이디는 4자 이상, 20자 이하로 입력해주세요.")
